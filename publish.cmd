07deccbd8fef0ab4fa5fa2d98e16b418eda7bd7b
juliognanfor <47969175+juliognanfor@users.noreply.github.com>
2019-06-24 10:58:24 +0200

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=bot0ejemplojgc-Web-Deploy.pubxml -p:Password=jkrKJ1HjimJuNenok6L2SRhwzJfmF8Glxr9CNlSv1Fk2zYPo6oqz95BEvRxl
+
